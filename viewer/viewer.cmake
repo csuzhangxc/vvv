aa55f047cd9c9900bc1d708af1aac8f93218910a
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2010-03-26 20:35:45 +0000

    add cmake lists

---
+++ viewer/viewer.cmake
@@ -0,0 +1,9 @@
+# v^3 header and module list
+
+SET(VIEWER_HDRS
+   ddsbase.h dicombase.h oglbase.h guibase.h tfbase.h tilebase.h
+   )
+
+SET(VIEWER_SRCS
+   ddsbase.cpp dicombase.cpp oglbase.cpp guibase.cpp tfbase.cpp tilebase.cpp
+   )
