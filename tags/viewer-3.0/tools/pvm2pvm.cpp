3886596df7558650e2a76cede82bbe374987a9e8
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2011-10-23 17:58:19 +0000

    minor change

---
+++ tags/viewer-3.0/tools/pvm2pvm.cpp
@@ -0,0 +1,49 @@
+#include "codebase.h"
+
+#include "ddsbase.h"
+
+int main(int argc,char *argv[])
+   {
+   const char info1[]="quantized from 16 bit to 8 bit by pvm2pvm tool";
+   const char info2[]="\nquantized from 16 bit to 8 bit by pvm2pvm tool";
+
+   unsigned char *volume,*volume2;
+
+   unsigned int width,height,depth,
+                components;
+
+   float scalex,scaley,scalez;
+
+   unsigned char *desc,*cour,*scan,*comm;
+
+   if (argc!=2 && argc!=3)
+      {
+      printf("usage: %s <16 bit input.pvm> [<8 bit output.pvm>]\n",argv[0]);
+      exit(1);
+      }
+
+   if ((volume=readPVMvolume(argv[1],&width,&height,&depth,&components,&scalex,&scaley,&scalez,&desc,&cour,&scan,&comm))==NULL) exit(1);
+
+   if (components==2)
+      {
+      volume2=quantize(volume,width,height,depth,TRUE,FALSE,TRUE);
+
+      if (comm!=NULL)
+         {
+         if ((comm=(unsigned char *)strdup((char *)comm))==NULL) ERRORMSG();
+         if ((comm=(unsigned char *)realloc(comm,strlen((char *)comm)+strlen(info2)+1))==NULL) ERRORMSG();
+         strcat((char *)comm,info2);
+         }
+      else
+         if ((comm=(unsigned char *)strdup(info1))==NULL) ERRORMSG();
+
+      if (argc>2) writePVMvolume(argv[2],volume2,width,height,depth,1,scalex,scaley,scalez,desc,cour,scan,comm);
+
+      free(volume2);
+      free(comm);
+      }
+
+   free(volume);
+
+   return(0);
+   }
