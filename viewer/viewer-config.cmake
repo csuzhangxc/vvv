193c5511757cf8cd8cb1414ad028156e2432eab7
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2011-10-31 09:49:38 +0000

    fix gcc version regexp

--- viewer/viewer-config.cmake
+++ viewer/viewer-config.cmake
@@ -21,7 +21,7 @@ ENDIF (NOT VIEWER_PATH)
 # gcc version
 IF (CMAKE_COMPILER_IS_GNUCXX)
    EXEC_PROGRAM(${CMAKE_CXX_COMPILER} ARGS --version OUTPUT_VARIABLE _compiler_output)
-   STRING(REGEX REPLACE ".* ([0-9]\\.[0-9]\\.[0-9]) .*" "\\1" GCC_COMPILER_VERSION ${_compiler_output})
+   STRING(REGEX REPLACE ".*([0-9]\\.[0-9]\\.[0-9]).*" "\\1" GCC_COMPILER_VERSION ${_compiler_output})
    MESSAGE(STATUS "gcc version: ${GCC_COMPILER_VERSION}")
 ENDIF (CMAKE_COMPILER_IS_GNUCXX)
 
