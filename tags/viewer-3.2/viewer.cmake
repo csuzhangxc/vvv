ddc414b2833c3e557f4890932468762a4ede7452
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2012-01-25 08:50:03 +0000

    tag viewer-3.2

---
+++ tags/viewer-3.2/viewer.cmake
@@ -0,0 +1,9 @@
+# v^3 header and module list
+
+SET(VIEWER_HDRS
+   ddsbase.h dicombase.h dirbase.h oglbase.h guibase.h tfbase.h tilebase.h
+   )
+
+SET(VIEWER_SRCS
+   ddsbase.cpp dicombase.cpp dirbase.cpp oglbase.cpp guibase.cpp tfbase.cpp tilebase.cpp
+   )
