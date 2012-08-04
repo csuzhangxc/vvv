e86bfe46ab871dfb47df83f4342144fe87a45654
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2012-08-04 15:18:25 +0000

    improve raw2pvm

--- viewer/tools/raw2pvm.cpp
+++ viewer/tools/raw2pvm.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc,char *argv[])
    {
-   unsigned char *data;
+   unsigned char *data,*ptr;
    unsigned int bytes;
 
    unsigned int width,height,depth;
@@ -15,7 +15,14 @@ int main(int argc,char *argv[])
    if (argc!=6 && argc!=7 && argc!=10)
       {
       printf("usage: %s <input.raw> <width> <height> <depth>\n",argv[0]);
-      printf("       [[-]<components> [<scalex> <scaley> <scalez>]] <output.pvm>\n");
+      printf("       [<components> [<scalex> <scaley> <scalez>]] <output.pvm>\n");
+      printf("components:\n");
+      printf(" 1=8bit\n");
+      printf(" 2=16bit/MSB\n");
+      printf(" -2=16bit/LSB\n");
+      printf(" -32768=16bit/LSB/signed\n");
+      printf(" 32767=16bit/MSB/signed\n");
+      printf(" 4=IEEE/float\n");
       exit(1);
       }
 
@@ -23,7 +30,7 @@ int main(int argc,char *argv[])
    if (sscanf(argv[3],"%d",&height)!=1) exit(1);
    if (sscanf(argv[4],"%d",&depth)!=1) exit(1);
    if (argc>=7) if (sscanf(argv[5],"%d",&components)!=1) exit(1);
-   if (width<1 || height<1 || depth<1 || (components<1 && components!=-2 && components!=-32768)) exit(1);
+   if (width<1 || height<1 || depth==0 || (components<1 && components!=-2 && components!=-32768)) exit(1);
 
    if (argc==10)
       {
@@ -32,16 +39,24 @@ int main(int argc,char *argv[])
       if (sscanf(argv[8],"%g",&scalez)!=1) exit(1);
       }
 
-   if ((data=readRAWfile(argv[1],&bytes))==NULL) exit(1);
-   if (components==-2) {swapbytes(data,bytes); components=2;}
-   if (components==-32768) {swapbytes(data,bytes); convbytes(data,bytes); components=2;}
-   if (components==32767) {convbytes(data,bytes); components=2;}
-   if (components==4) {convfloat(data,bytes); components=2;}
+   if ((data=ptr=readRAWfile(argv[1],&bytes))==NULL) exit(1);
+
+   if (depth<0)
+      {
+      depth=-depth;
+      if (components>0) ptr=&data[bytes-width*height*depth*components];
+      else ptr=&data[bytes-width*height*depth*2];
+      }
+
+   if (components==-2) {swapbytes(ptr,bytes); components=2;}
+   if (components==-32768) {swapbytes(ptr,bytes); convbytes(ptr,bytes); components=2;}
+   if (components==32767) {convbytes(ptr,bytes); components=2;}
+   if (components==4) {convfloat(ptr,bytes); components=2; bytes/=2;}
    if (bytes<width*height*depth*components) exit(1);
 
-   if (argc==6) writePVMvolume(argv[5],data,width,height,depth,components);
-   else if (argc==7) writePVMvolume(argv[6],data,width,height,depth,components);
-   else writePVMvolume(argv[9],data,width,height,depth,components,scalex,scaley,scalez);
+   if (argc==6) writePVMvolume(argv[5],ptr,width,height,depth,components);
+   else if (argc==7) writePVMvolume(argv[6],ptr,width,height,depth,components);
+   else writePVMvolume(argv[9],ptr,width,height,depth,components,scalex,scaley,scalez);
    free(data);
 
    return(0);
