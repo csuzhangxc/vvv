b04ae741f71e91a9bba7e5a95592ea0392e525a3
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2012-08-05 18:52:00 +0000

    read volume dimensions from .rek file

--- viewer/tools/rek2raw.cpp
+++ viewer/tools/rek2raw.cpp
@@ -4,44 +4,64 @@ static const double gamma_correction=0.5;
 
 int main(int argc,char *argv[])
    {
+   unsigned int i;
+
    FILE *file,*out;
 
    unsigned char data[2];
 
-   unsigned int width,height,depth;
+   unsigned int width,height,depth,bits;
 
    int v;
    double vg;
    unsigned char vc;
 
-   if (argc!=6)
+   if (argc!=3)
       {
-      printf("usage: %s <input.rek> <width> <height> <depth> <output.raw>\n",argv[0]);
+      printf("usage: %s <input.rek> <output.raw>\n",argv[0]);
       printf(" input: 16bit fraunhofer volume file format (with 2048 byte header)\n");
       printf(" output: 8bit raw volume\n");
       exit(1);
       }
 
-   if (sscanf(argv[2],"%d",&width)!=1) exit(1);
-   if (sscanf(argv[3],"%d",&height)!=1) exit(1);
-   if (sscanf(argv[4],"%d",&depth)!=1) exit(1);
-   if (width<1 || height<1 || depth<1) exit(1);
-
    if ((file=fopen(argv[1],"rb"))==NULL) exit(1);
-   if (fseek(file,-width*height*depth*2,SEEK_END)==-1) exit(1);
 
-   if ((out=fopen(argv[5],"wb"))==NULL) exit(1);
+   if (fread(&data,1,2,file)!=2) exit(1);
+   width=data[0]+256*data[1];
 
-   // totally unoptimized:
-   //  one 16bit LSB value in
-   //  one gamma-corrected 8bit value out
-   while (fread(&data,1,2,file)==2)
+   if (fread(&data,1,2,file)!=2) exit(1);
+   height=data[0]+256*data[1];
+
+   if (fread(&data,1,2,file)!=2) exit(1);
+   bits=data[0]+256*data[1];
+
+   if (fread(&data,1,2,file)!=2) exit(1);
+   depth=data[0]+256*data[1];
+
+   if (width<1 || height<1 || depth<1) exit(1);
+   if (bits!=8 && bits!=16) exit(1);
+
+   if (fseek(file,2048,SEEK_SET)==-1) exit(1);
+
+   if ((out=fopen(argv[2],"wb"))==NULL) exit(1);
+
+   for (i=0; i<width*height*depth; i++)
       {
-      v=data[0]+256*data[1];
-      vg=pow(v/65535.0,gamma_correction);
-      vc=ftrc(vg*255.0+0.5);
+      if (bits==8)
+         {
+         if (fread(&data,1,1,file)!=1) exit(1);
+         vc=data[0];
+         }
+      else
+         {
+         if (fread(&data,1,2,file)!=2) exit(1);
 
-      fwrite(&vc,1,1,out); 
+         v=data[0]+256*data[1];
+         vg=pow(v/65535.0,gamma_correction);
+         vc=ftrc(vg*255.0+0.5);
+         }
+
+      fwrite(&vc,1,1,out);
       }
 
    fclose(file);
