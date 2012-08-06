eb06826633864bc94b9155c03e5c42841f56e53c
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2012-08-06 06:50:05 +0000

    output rek volume to raw as is

--- viewer/tools/rek2raw.cpp
+++ viewer/tools/rek2raw.cpp
@@ -12,15 +12,11 @@ int main(int argc,char *argv[])
 
    unsigned int width,height,depth,bits;
 
-   int v;
-   double m;
-   unsigned char c;
-
    if (argc!=3)
       {
       printf("usage: %s <input.rek> <output.raw>\n",argv[0]);
-      printf(" input: 16bit fraunhofer volume file format (with 2048 byte header)\n");
-      printf(" output: 8bit raw volume\n");
+      printf(" input: 8bit or 16bit fraunhofer volume file format (with 2048 byte header)\n");
+      printf(" output: 8bit or 16bit MSB raw volume\n");
       exit(1);
       }
 
@@ -41,30 +37,24 @@ int main(int argc,char *argv[])
    if (width<1 || height<1 || depth<1) exit(1);
    if (bits!=8 && bits!=16) exit(1);
 
-   printf("found volume with dimensions %dx%dx%d\n",width,height,depth);
+   printf("found volume with dimensions %dx%dx%d and %d bits\n",width,height,depth,bits);
 
    if (fseek(file,2048,SEEK_SET)==-1) exit(1);
 
    if ((out=fopen(argv[2],"wb"))==NULL) exit(1);
 
    for (i=0; i<width*height*depth; i++)
-      {
       if (bits==8)
          {
-         if (fread(&data,1,1,file)!=1) exit(1);
-         c=data[0];
+         if (fread(data,1,1,file)!=1) exit(1);
+         fwrite(data,1,1,out);
          }
       else
          {
-         if (fread(&data,1,2,file)!=2) exit(1);
-
-         v=data[0]+256*data[1];
-         m=1.0-pow(1.0-v/65535.0,exponent);
-         c=ftrc(m*255.0+0.5);
+         if (fread(data,1,2,file)!=2) exit(1);
+         fwrite(data+1,1,1,out);
+         fwrite(data,1,1,out);
          }
-
-      fwrite(&c,1,1,out);
-      }
 
    fclose(file);
    fclose(out);
