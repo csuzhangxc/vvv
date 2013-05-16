a4ff6236b13611b1b30ac4d0db80e7f4be93b2a7
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2013-05-16 20:02:12 +0000

    split lib into glut and opengl parts

--- viewer/viewer.cmake
+++ viewer/viewer.cmake
@@ -1,9 +1,17 @@
 # v^3 header and module list
 
+SET(VOLREN_HDRS
+   ddsbase.h dicombase.h dirbase.h oglbase.h tfbase.h tilebase.h volume.h volren.h
+   )
+
+SET(VOLREN_SRCS
+   ddsbase.cpp dicombase.cpp dirbase.cpp oglbase.cpp tfbase.cpp tilebase.cpp volume.cpp
+   )
+
 SET(VIEWER_HDRS
-   ddsbase.h dicombase.h dirbase.h oglbase.h glutbase.h guibase.h tfbase.h tilebase.h volume.h volren.h
+   glutbase.h guibase.h
    )
 
 SET(VIEWER_SRCS
-   ddsbase.cpp dicombase.cpp dirbase.cpp oglbase.cpp glutbase.cpp guibase.cpp tfbase.cpp tilebase.cpp volume.cpp
+   glutbase.cpp guibase.cpp
    )
