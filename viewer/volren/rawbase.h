d8534c32db57a8508c36dc5ee92de4040d4574b0
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2013-05-28 16:22:05 +0000

    add plain copy method

--- viewer/volren/rawbase.h
+++ viewer/volren/rawbase.h
@@ -45,11 +45,18 @@ BOOLINT writeRAWvolume(const char *filename, // /wo suffix .raw
                        unsigned int components=1,unsigned int bits=8,BOOLINT sign=FALSE,BOOLINT msb=TRUE,
                        float scalex=1.0f,float scaley=1.0f,float scalez=1.0f);
 
-// copy a RAW volume with out-of-core linear quantization
+// copy a RAW volume
 BOOLINT copyRAWvolume(FILE *file, // source file
                       const char *output, // destination file name /wo .raw
                       unsigned int width,unsigned int height,unsigned int depth=1,unsigned int steps=1,
                       unsigned int components=1,unsigned int bits=8,BOOLINT sign=FALSE,BOOLINT msb=TRUE,
                       float scalex=1.0f,float scaley=1.0f,float scalez=1.0f);
+
+// copy a RAW volume with out-of-core linear quantization
+BOOLINT copyRAWvolume_linear(FILE *file, // source file
+                             const char *output, // destination file name /wo .raw
+                             unsigned int width,unsigned int height,unsigned int depth=1,unsigned int steps=1,
+                             unsigned int components=1,unsigned int bits=8,BOOLINT sign=FALSE,BOOLINT msb=TRUE,
+                             float scalex=1.0f,float scaley=1.0f,float scalez=1.0f);
 
 #endif
