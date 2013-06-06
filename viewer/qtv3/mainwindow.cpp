de31ff5119101fe0b0c268cd01831e66e243ffb6
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2013-06-06 10:39:56 +0000

    tweak version

--- viewer/qtv3/mainwindow.cpp
+++ viewer/qtv3/mainwindow.cpp
@@ -5,7 +5,7 @@
 #include "mainwindow.h"
 
 #define APP_NAME "QTV3"
-#define APP_VERSION "0.9"
+#define APP_VERSION "0.9.1"
 
 QTV3MainWindow::QTV3MainWindow(QWidget *parent)
    : QMainWindow(parent)
