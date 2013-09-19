b60d7ff93bd62c7a4a685aac5c796394d299d38c
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2013-09-19 09:48:32 +0000

    retag v3.7.2

---
+++ tags/viewer-3.7.2/tools/rawquant.cpp
@@ -0,0 +1,26 @@
+#include "codebase.h"
+#include "rawbase.h"
+
+static const float ratio=0.5f;
+static const unsigned long long maxcells=250000000;
+
+int main(int argc,char *argv[])
+   {
+   char *output;
+
+   if (argc!=2)
+      {
+      printf("usage: %s <input.raw>\n",argv[0]);
+      exit(1);
+      }
+
+   output=processRAWvolume(argv[1],ratio,maxcells);
+
+   if (output)
+      {
+      printf("wrote %s\n",output);
+      free(output);
+      }
+
+   return(0);
+   }
---
+++ tags/viewer-3.7.2/viewer.cmake
@@ -0,0 +1,24 @@
+# v^3 header and module list
+
+SET(VOLREN_HDRS
+   volren/codebase.h
+   volren/ddsbase.h volren/dicombase.h volren/rekbase.h volren/rawbase.h
+   volren/dirbase.h volren/oglbase.h
+   volren/tfbase.h volren/tilebase.h volren/progs.h
+   volren/volume.h volren/volren.h
+   )
+
+SET(VOLREN_SRCS
+   volren/ddsbase.cpp volren/dicombase.cpp volren/rekbase.cpp volren/rawbase.cpp
+   volren/dirbase.cpp volren/oglbase.cpp
+   volren/tfbase.cpp volren/tilebase.cpp
+   volren/volume.cpp
+   )
+
+SET(VIEWER_HDRS
+   glutbase.h guibase.h
+   )
+
+SET(VIEWER_SRCS
+   glutbase.cpp guibase.cpp
+   )
