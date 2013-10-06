caed6738c3adc1b2c717290007406971e7d882a8
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2013-10-06 16:48:41 +0000

    parametrize cell limit

--- viewer/tools/rawquant.cpp
+++ viewer/tools/rawquant.cpp
@@ -5,27 +5,35 @@
 #endif
 
 static const float ratio=0.5f;
-static const unsigned long long maxcells=250000000;
+static const unsigned long long maxcells=250;
 
 int main(int argc,char *argv[])
    {
-   if (argc!=2)
+   unsigned long long cell_limit=maxcells;
+
+   if (argc!=2 && argc!=3)
       {
-      printf("usage: %s <input.raw>\n",argv[0]);
+      printf("usage: %s <input.raw> [<volume cell limit in mb>]\n",argv[0]);
       exit(1);
       }
+
+   if (argc==3)
+      if (sscanf(argv[2],"%llu",&cell_limit)!=1) exit(1);
+
+   cell_limit*=1000000;
 
 #ifdef HAVE_MINI
 
    char *output;
 
-   output=processRAWvolume(argv[1],ratio,maxcells);
+   output=processRAWvolume(argv[1],ratio,cell_limit);
 
    if (output)
       {
       printf("wrote %s\n",output);
       free(output);
       }
+   else exit(1);
 
 #else
 
