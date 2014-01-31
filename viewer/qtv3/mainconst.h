98f8c00927fe1254e96e1e16883c23d335b45f9a
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2014-01-31 08:40:48 +0000

    tweak main window height so that slider groups fits better

--- viewer/qtv3/mainconst.h
+++ viewer/qtv3/mainconst.h
@@ -4,13 +4,13 @@
 #define MAINCONST_H
 
 #define APP_NAME "QTV3"
-#define APP_VERSION "2.0 as of 30.January.2014"
+#define APP_VERSION "2.0.1 as of 31.January.2014"
 
 #define APP_LICENSE "licensed under GPL 3.0"
 #define APP_COPYRIGHT "(c) by Stefan Roettger 2013-2014"
 #define APP_DISCLAIMER "no warranty, no responsibility, no nothing\nis granted for anything!"
 
 #define MAIN_WIDTH 900
-#define MAIN_HEIGHT 600
+#define MAIN_HEIGHT 700
 
 #endif
