8dddcdda29a07b735544bc35ec7d1efb7256cf11
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2014-02-01 12:34:34 +0000

    exit if installation was unsuccessful

--- viewer/install.sh
+++ viewer/install.sh
@@ -3,10 +3,14 @@
 
 echo compiling libmini...
 (cd mini; cmake . && make -j 4)
+if ($? != 0) exit($?)
 echo compiling libvolren...
 cmake . && make -j 4
+if ($? != 0) exit($?)
 echo compiling qtv3...
 (cd qtv3; cmake . && make -j 4)
+if ($? != 0) exit($?)
 echo installing qtv3...
 echo enter root password when prompted
 (cd qtv3; sudo make install)
+if ($? != 0) exit($?)
