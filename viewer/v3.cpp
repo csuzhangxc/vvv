273612c7a1c3d1d3b3295981dc7f624feb3e465e
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2014-06-05 06:49:04 +0000

    bump version

--- viewer/v3.cpp
+++ viewer/v3.cpp
@@ -1,6 +1,6 @@
 // (c) by Stefan Roettger, licensed under GPL 2+
 
-#define VERSION "4.3.2 as of 3.June.2014"
+#define VERSION "4.4 as of 5.June.2014"
 
 #include "codebase.h" // universal code base
 #include "oglbase.h" // OpenGL base rendering
