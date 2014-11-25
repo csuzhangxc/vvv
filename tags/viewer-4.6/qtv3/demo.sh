294174dbc020dd87fd39cc0f9114b63d3786a165
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2014-11-25 17:42:27 +0000

    remove old tags

--- tags/viewer-4.6/qtv3/demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/tcsh -f
-
-set volume=Bucky.pvm
-if ($1 != "") set volume=$1
-
-set qtv3=./qtv3
-if ($HOSTTYPE == "intel-pc") set qtv3=qtv3.app/Contents/MacOS/qtv3
-
-$qtv3 --demo --fullscreen --hue=120 --zoom=40 --tfcenter=0.75 --tfsize=1.25 --tfinv --tfemi=100 --tfatt=50 --maxidle=60 --maxvol=1024 --maxiso=256 $volume
