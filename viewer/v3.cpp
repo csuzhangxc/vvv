263c5ac59aaed786a0a44ce7cc959fde364b193c
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2014-12-03 11:57:31 +0000

    fix Windows build

--- viewer/v3.cpp
+++ viewer/v3.cpp
@@ -1,6 +1,6 @@
 // (c) by Stefan Roettger, licensed under GPL 2+
 
-#define VERSION "5.0 as of 19.November.2014"
+#define VERSION "5.0 as of 3.December.2014"
 
 #include "codebase.h" // universal code base
 #include "oglbase.h" // OpenGL base rendering
