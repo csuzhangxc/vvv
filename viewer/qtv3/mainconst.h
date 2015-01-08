7e594cfb7d67c899553802eee5017753b2dfb035
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2015-01-08 07:02:29 +0000

    update version and copyright

--- viewer/qtv3/mainconst.h
+++ viewer/qtv3/mainconst.h
@@ -4,10 +4,10 @@
 #define MAINCONST_H
 
 #define APP_NAME "QTV3"
-#define APP_VERSION "3.0 as of 18.December.2014"
+#define APP_VERSION "3.0 as of 8.January.2015"
 
 #define APP_LICENSE "licensed under GPL 3.0"
-#define APP_COPYRIGHT "(c) by Stefan Roettger 2013-2014"
+#define APP_COPYRIGHT "(c) by Stefan Roettger 2013-2015"
 #define APP_DISCLAIMER "no warranty, no responsibility, no nothing\nis granted for anything!"
 
 #define MAIN_WIDTH 900
