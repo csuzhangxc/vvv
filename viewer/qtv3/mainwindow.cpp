dca7b818895cef6c26a1fe8e1919af5783e6ec02
stefan@stereofx.org <stefan@stereofx.org@08707dae-eed3-11dd-abe2-393a09110dd0>
2015-01-12 10:36:03 +0000

    tweak qss

--- viewer/qtv3/mainwindow.cpp
+++ viewer/qtv3/mainwindow.cpp
@@ -360,7 +360,9 @@ void QTV3MainWindow::createWidgets()
    // set main inherited style sheet
    QString css("QGroupBox { background-color: #eeeeee; border: 2px solid #999999; border-radius: 5px; }"
                "QGroupBox#mainGroupBox { border: 0; border-radius: 0; }"
-               "QGroupBox#viewerGroupBox { background-color: #d9d9d9; }");
+               "QGroupBox#viewerGroupBox { background-color: #d9d9d9; }"
+               "QSplitter::handle:horizontal { background-color: #d9d9d9; }"
+               ":focus { background-color: white; }");
    if (demo_) css+="QGroupBox#viewerGroupBox { border: 0; border-radius: 0; }";
    mainSplitter_->setStyleSheet(css);
 
